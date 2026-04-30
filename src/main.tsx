e0331f71279208f44b4e090600fb7524b2f4c4f0
Andres Badillo <r.andres.badillo@gmail.com>
2026-04-30 14:07:55 -0500

    Se agregaron speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { HelmetProvider } from "react-helmet-async";
 import { PostsProvider } from "@/context/PostsContext";
 import { App } from "./App";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <PostsProvider>
         <App />
         <Analytics />
+        <SpeedInsights />
       </PostsProvider>
     </HelmetProvider>
   </React.StrictMode>,
